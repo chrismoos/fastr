26d84d2890d178d0f2a767b0f5b1ca513eadf031
evizitei <ethan.vizitei@gmail.com>
2010-06-10 12:27:48 -0400

    move APP_PATH and altered application classes into helper to be used by other tests
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- test/helper.rb
+++ test/helper.rb
@@ -6,8 +6,27 @@ require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+APP_PATH = "#{File.expand_path(File.dirname(__FILE__))}/fastr_app"
+
 require 'fastr'
 EM.kqueue = true
+
+class NueteredBootingApplication < Fastr::Application
+  attr_reader :booted
+  def boot
+    @booted = true
+  end
+end
+
+class ManualBootingApplication < Fastr::Application
+  include Fastr::Log
+  
+  def initialize(path)
+    self.app_path = path
+    @booting = true
+  end
+end
 
 class Test::Unit::TestCase
   def em_setup 
--- test/test_application.rb
+++ test/test_application.rb
@@ -1,21 +1,20 @@
 require 'helper'
 
 class TestApplication < Test::Unit::TestCase
-  APP_PATH = "#{File.expand_path(File.dirname(__FILE__))}/fastr_app"
     
-    context "New Application" do  
-      setup do
-        em_setup{ @application = NueteredBootingApplication.new("/some/path") }
-      end
-      
-      should "store startup path" do
-        assert_equal "/some/path",@application.app_path
-      end
-      
-      should "boot application" do
-        assert @application.booted
-      end
+  context "New Application" do  
+    setup do
+      em_setup{ @application = NueteredBootingApplication.new("/some/path") }
     end
+    
+    should "store startup path" do
+      assert_equal "/some/path",@application.app_path
+    end
+    
+    should "boot application" do
+      assert @application.booted
+    end
+  end
     
   context "Application Boot" do
     setup do
@@ -36,21 +35,5 @@ class TestApplication < Test::Unit::TestCase
     should "setup routes from route file" do
       assert_equal 3,@application.router.routes.size
     end
-  end
-end
-
-class NueteredBootingApplication < Fastr::Application
-  attr_reader :booted
-  def boot
-    @booted = true
-  end
-end
-
-class ManualBootingApplication < Fastr::Application
-  include Fastr::Log
-  
-  def initialize(path)
-    self.app_path = path
-    @booting = true
   end
 end
