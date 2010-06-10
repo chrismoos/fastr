e2d1f753600a2b09d574f345ba7f246d67c5a32e
evizitei <ethan.vizitei@gmail.com>
2010-06-10 11:50:03 -0400

    include mocha for stubbing in tests
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- test/helper.rb
+++ test/helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'eventmachine'
 require 'test/unit'
 require 'shoulda'
+require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
