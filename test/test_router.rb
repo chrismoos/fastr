805b728bb1dd4ab66099126019153355c6a5f8e6
evizitei <ethan.vizitei@gmail.com>
2010-06-10 15:33:33 -0400

    new route parsing tests for the router
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- test/test_router.rb
+++ test/test_router.rb
@@ -16,13 +16,57 @@ class TestRouter < Test::Unit::TestCase
       should "infer directory of routes file" do
         assert_equal File.join(APP_PATH,"/app/config/routes.rb"),@router.route_file
       end
+      
+      context "when parsing mapping" do
+        context "for standard controller" do
+          setup { parse_route('/:controller/:action') }
+        
+          should "build proper regex" do
+            assert_equal "^/(\\w+)/(\\w+)$",@route_map[:regex]
+          end
+        
+          should "have no extra arguments" do
+            assert_nil @route_map[:args]
+          end
+        
+          should "have var for controller and action" do
+            assert_equal [:controller,:action],@route_map[:vars]
+          end
+        
+          should "have an empty hash" do
+            assert @route_map[:hash].empty?
+          end
+        end
+        
+        context "with a regex match" do
+          setup { parse_route("/home/:action",:action => '[A-Za-z]+') }
+          
+          should "build proper regex" do
+            assert_equal "^/home/([A-Za-z]+)$",@route_map[:regex]
+          end
+        
+          should "have one argument for the action regex" do
+            assert_equal({:action=>"[A-Za-z]+"},@route_map[:args])
+          end
+        
+          should "have var for the action matcher" do
+            assert_equal [:action],@route_map[:vars]
+          end
+        
+          should "have an empty hash" do
+            assert @route_map[:hash].empty?
+          end
+        end
+        
+        context "with an exact match" do
+          setup { parse_route("'/home/:action'",:to => 'home#index') }
+        end
+      end
     end 
-    
-    # context "route parser" do
-    #       setup do
-    #         @router = Fastr::Router.new(NueteredBootingApplication.new(APP_PATH))
-    #       end
-    #     end
   end
- 
+  
+  def parse_route(route,*args)
+    @router.for(route,*args)
+    @route_map = @router.routes.first
+  end 
 end
