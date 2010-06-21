f2b4b3c9503b71ed3eb8e20a50e71f7e443fd57e
Chris Moos <chris@tech9computers.com>
2010-06-20 17:58:39 -0700

    Added docs for cookie.

--- lib/fastr/cookie.rb
+++ lib/fastr/cookie.rb
@@ -1,5 +1,17 @@
 module Fastr
+  # This module adds helpers for handling cookies.
+  #
+  # == Setting a cookie
+  # 
+  #   set_cookie("mycookie", "value", {:expires => Time.now + 3600})
+  #
+  # @author Chris Moos
   module Cookie
+    # Adds a <em>Set-Cookie</em> header in the response.
+    # 
+    # @param key [String]
+    # @param value [String]
+    # @options options [Time] :expires The time when the cookie should expire.
     def set_cookie(key, value, options={})
       cookie = ["#{key}=#{value};"]
       
