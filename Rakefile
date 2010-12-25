c5a4c37e3acfeee705a2d72579264e5c32e7b0ab
Chris Moos <chris@tech9computers.com>
2010-12-25 16:56:31 -0700

    Fixed inconsistency in cookie test.

--- Rakefile
+++ Rakefile
@@ -43,8 +43,6 @@ rescue LoadError
   end
 end
 
-task :test => :check_dependencies
-
 task :default => :test
 
 require 'rake/rdoctask'
--- lib/fastr/cookie.rb
+++ lib/fastr/cookie.rb
@@ -2,30 +2,31 @@ module Fastr
   # This module adds helpers for handling cookies.
   #
   # == Setting a cookie
-  # 
+  #
   #   set_cookie("mycookie", "value", {:expires => Time.now + 3600})
   #
   # @author Chris Moos
   module Cookie
     # Adds a <em>Set-Cookie</em> header in the response.
-    # 
+    #
     # @param key [String]
     # @param value [String]
     # @option options [Time] :expires The time when the cookie should expire.
     def set_cookie(key, value, options={})
       cookie = ["#{key}=#{value};"]
-      
-      
+
+
       if options.has_key? :expires and options[:expires].kind_of? Time
         options[:expires] = options[:expires].utc.strftime('%a, %d-%b-%Y %H:%M:%S GMT')
       end
-      
-      options.each do |k,v|
+
+      # Sort the cookies alphabetically.
+      options.sort { |a,b| a.to_s <=> b.to_s }.each do |k,v|
         cookie << "#{k}=#{v.to_s};"
       end
 
       cookie_val = cookie.join(' ')
-      
+
       if self.headers['Set-Cookie'].nil?
         self.headers['Set-Cookie'] = [cookie_val]
       else
